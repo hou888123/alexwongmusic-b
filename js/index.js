8b9900cf3999ab234e674b949c838031855f5dcc
hou888123 <74288372+hou888123@users.noreply.github.com>
2022-08-17 18:53:04 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,36 +2,28 @@ $(function(){
     $('.right_menu_btn,.menu_bg,.right_menu a').on('click',function(){
         $('.right_menu,.menu_bg,.right_menu_btn').toggleClass('open');
     });
-    $(".content").overlayScrollbars({
-        className:'os-theme-dark',
-        callbacks :{
-            onScroll : (e)=> {
-                e.target.scrollTop? $('header,.go_top').addClass('onScroll') :$('header,.go_top').removeClass('onScroll');
-            },
-            onScrollStop : (e)=> {
-                document.cookie = "scrollTop=" + e.target.scrollTop;
-            },
-            onInitialized : function() {
-                if (document.cookie.match(/scrollTop=([^;]+)(;|$)/) != null) {  
-                    var arr = document.cookie.match(/scrollTop=([^;]+)(;|$)/); //cookies中不為空，則讀取滾動條位置  
-                    $(this)[0].scroll([ 0, parseInt(arr[1]) ], 0);
-                }  
-            }
-        }
+    $(window).scroll(function(){
+        $(this).scrollTop()?$('header,.go_top').addClass('onScroll') :$('header,.go_top').removeClass('onScroll');
     });
     $('.go_top').on('click',function(){
-        var instance = OverlayScrollbars($(".content")[0]); 
-        instance.scroll([ 0, 0], 300);
+        $('html, body').animate({scrollTop:0}, 300);
     });
     $('.go').on('click',function(){
-        var instance = OverlayScrollbars($(".content")[0]); 
-        instance.scroll([ 0, $(`._${$(this).attr('class').split("_")[1]}`)[0].offsetTop - $("header").height() ], 300);
+        $('html, body').animate({scrollTop:$(`._${$(this).attr('class').split('_')[1]}`)[0].offsetTop - $('header').height()}, 300);
     });
     $('.scroll_down').on('click',function(){
-        var instance = OverlayScrollbars($(".content")[0]); 
-        instance.scroll([ 0, $(`._news`)[0].offsetTop/1.2 - $("header").height() ], 300);
+        $('html, body').animate({scrollTop:$('._news')[0].offsetTop/1.2 - $('header').height()}, 300);
     });
+    if (document.cookie.match(/scrollTop=([^;]+)(;|$)/) != null) {  
+        var arr = document.cookie.match(/scrollTop=([^;]+)(;|$)/); //cookies中不為空，則讀取滾動條位置  
+        $(window).scrollTop(parseInt(arr[1]));
+    }  
 });
-$(window).load(function() {
-     window.scrollTo(0, document.querySelector(".content").scrollHeight);
-});
+
+var isScrolling;
+window.addEventListener('scroll', function ( event ) {
+	window.clearTimeout( isScrolling );
+	isScrolling = setTimeout(function() {
+		document.cookie = 'scrollTop=' + $(this).scrollTop();
+	}, 66);
+}, false);
